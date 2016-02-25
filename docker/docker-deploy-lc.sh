beb08df6fd736bd3d656a5efdb2ba42566a63e6b
boris <boris@gigaspaces.com>
2016-02-25 12:36:23 +0200

    fixed docker scripts

--- docker/docker-deploy-lc.sh
+++ docker/docker-deploy-lc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ../
+chmod 400 /mnt/${PEM_FILE_NAME}
 ansible-playbook -i hosts --private-key /mnt/${PEM_FILE_NAME} site.yml
--- docker/run.sh
+++ docker/run.sh
@@ -6,4 +6,4 @@ if [ $# -ne 4 ]; then
 fi
 
 echo "Running large cluster!"
-docker run -v `pwd`/mnt:/mnt --env AWS_ACCESS_KEY_ID=$1 --env AWS_SECRET_ACCESS_KEY=$2 --env AWS_DEFAULT_REGION=$3 --env PEM_FILE_NAME=$4 -it newman/anisble /bin/bash -c "cd /lc-ansible && git pull && cp -f /mnt/all /lc-ansible/group_vars && /lc-ansible/docker/docker-deploy-lc.sh"
+docker run -v `pwd`/mnt:/mnt --env AWS_ACCESS_KEY_ID=$1 --env AWS_SECRET_ACCESS_KEY=$2 --env AWS_DEFAULT_REGION=$3 --env PEM_FILE_NAME=$4 -it lc/anisble /bin/bash -c "cd /lc-ansible && git pull && cp -f /mnt/all /lc-ansible/group_vars && cd docker && ./docker-deploy-lc.sh"
