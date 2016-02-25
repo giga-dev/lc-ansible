273cfb41ae7f9698e8faad962d5c3ccb1307ad98
boris <boris@gigaspaces.com>
2016-02-25 13:16:12 +0200

    don't ask for ssh confirmation

--- docker/docker-deploy-lc.sh
+++ docker/docker-deploy-lc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../
 chmod 400 /mnt/${PEM_FILE_NAME}
+sed -i "s/#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
 ansible-playbook -i hosts --private-key /mnt/${PEM_FILE_NAME} site.yml
